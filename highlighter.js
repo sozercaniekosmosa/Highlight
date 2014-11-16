5d6df2880d10324843b186b165935ca435d28594
sozercaniekosmosa <sozercaniekosmosa@gmail.com>
2014-11-16 15:07:48 +0600

    Проект
    первый

---
+++ highlighter.js
@@ -0,0 +1,18 @@
+var isHighlight = false;
+
+document.body.addEventListener('mousewheel', function () {
+	var select = window.getSelection() + '';
+	if ((select == '') || (select.length > 110)) return;
+
+	var html = document.body.innerHTML.split(select).join('<x>' + select + '</x>');
+	document.body.innerHTML = html;
+	isHighlight = true;
+}, false);
+
+document.body.addEventListener('mousedown', function () {
+	if (!isHighlight) return;
+	var html = document.body.innerHTML.split('<x>').join('');
+	html = html.split('</x>').join('');
+	document.body.innerHTML = html;
+	isHighlight = false;
+}, false);
